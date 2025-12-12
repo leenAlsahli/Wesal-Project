3f59a5f2c614d4c2452e535fe73c9a4d8142a515
Wesal Help Desk Management System <wesal@helpdesk.com>
2025-12-12 22:29:15 +0300

    Add query4.sql inside queries folder

---
+++ queries/query4.sql
@@ -0,0 +1,16 @@
+--querey 4: الاستعلام عن التذاكر العاجله المسنده الى لين
+select 
+    t.ticket_id,  
+    t.title as ticket_title,
+    t.priority,
+    u.first_name as user_name, 
+    d.name as department
+from public.ticket t
+
+join users u on t.user_id = u.user_id
+join support_agent sa on t.sagent_id = sa.sagent_id
+join
+    departments d on t.dept_id = d.dept_id
+where 
+    sa.f_name = 'Leen' 
+    and t.priority = 'urgent';
---
+++ queries/query5.sql
@@ -0,0 +1,8 @@
+--querey 5: عد اجمالي التذاكر حسب الحالة
+select
+    Status as Ticket_Status, 
+    COUNT(Ticket_ID) as Total_Count
+from
+    Ticket
+group by
+    Status;
